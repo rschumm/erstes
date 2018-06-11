822c7d0bf69d2ba8e146f2332fd9427f75494928
Rémy Schumm <dev@schumm.ch>
2018-06-11 07:07:28 +0000

    für Robert

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  static final String template = "Guten Tag Sie Siemens-Mensch %s!";
+  static final String template = "Guten Tag Sie Noser-Mensch %s!";
 
   @Override
   public void start(Future<Void> future) {
